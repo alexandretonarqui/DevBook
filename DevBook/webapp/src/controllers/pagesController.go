d249dea13d0c97b41811abfc97af4bd83f6a723f
alexandretonarqui <alexandre.tonarqui@gmail.com>
2026-09-03 03:26:23 -0300

    Identificando o Usuário Logado

--- DevBook/webapp/src/controllers/pagesController.go
+++ DevBook/webapp/src/controllers/pagesController.go
@@ -4,24 +4,26 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"webapp/src/config"
+	"webapp/src/cookies"
 	"webapp/src/models"
 	"webapp/src/requests"
 	"webapp/src/responses"
 	"webapp/src/utils"
 )
 
-//LoadLoginPage vai carregar a tela de Login
+// LoadLoginPage vai carregar a tela de Login
 func LoadLoginPage(w http.ResponseWriter, r *http.Request) {
 	utils.ExecTemplate(w, "login.html", nil)
 }
 
-//LoadUserSubmitPage vai carregar a página de cadastro de usuário
+// LoadUserSubmitPage vai carregar a página de cadastro de usuário
 func LoadUserSubmitPage(w http.ResponseWriter, r *http.Request) {
 	utils.ExecTemplate(w, "registration.html", nil)
 }
 
-//LoadHomePage carrega a página principal com as publicações
+// LoadHomePage carrega a página principal com as publicações
 func LoadHomePage(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/publications", config.APIURL)
 	response, erro := requests.MakeAuthRequest(r, http.MethodGet, url, nil)
@@ -35,12 +37,21 @@ func LoadHomePage(w http.ResponseWriter, r *http.Request) {
 		responses.TreatStatusCodeError(w, response)
 		return
 	}
-	
+
 	var publications []models.Publication
 	if erro = json.NewDecoder(response.Body).Decode(&publications); erro != nil {
 		responses.JSON(w, http.StatusUnprocessableEntity, responses.ErroAPI{Erro: erro.Error()})
 		return
 	}
 
-	utils.ExecTemplate(w, "home.html", publications)
+	cookie, _ := cookies.Read(r)
+	userID, _ := strconv.ParseUint(cookie["id"], 10, 64)
+
+	utils.ExecTemplate(w, "home.html", struct {
+		Publications []models.Publication
+		UserID       uint64
+	}{
+		Publications: publications,
+		UserID:       userID,
+	})
 }
