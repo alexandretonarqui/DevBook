7fc32c03cd7abd17ee39c29b04a6be02f622098f
alexandretonarqui <alexandre.tonarqui@gmail.com>
2026-09-06 21:41:02 -0300

    Busca de Usuário por ID - Utilizando Concurrence

--- DevBook/webapp/src/models/userModel.go
+++ DevBook/webapp/src/models/userModel.go
@@ -1,8 +1,12 @@
 package models
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
+	"webapp/src/config"
+	"webapp/src/requests"
 )
 
 // User representa uma pessoa utilizando a rede social
@@ -17,32 +21,92 @@ type User struct {
 	Publications []Publication `json:"publications"`
 }
 
-//SearchFullUser faz 4 requisições na API para montar o usuário
-func SearchFullUser(userID, uint64, r http.Request) (User, error){
+// SearchFullUser faz 4 requisições na API para montar o usuário
+func SearchFullUser(userID, uint64, r http.Request) (User, error) {
 	userChannel := make(chan User)
 	followersChannel := make(chan []User)
 	followingChannel := make(chan []User)
 	publicationsChannel := make(chan []Publication)
 
 	go GetUserData(userChannel, userID, r)
-	go GetFollowers(followersChannel,userID, r)
+	go GetFollowers(followersChannel, userID, r)
 	go GetFollowing(followingChannel, userID, r)
 	GetPublications(publicationsChannel, userID, r)
 
 }
 
-func GetUserData(channel <- chan User, userID uint64, r *http.Request) {
+// GetUserData chama a API para buscar os dados base do usuário
+func GetUserData(channel chan<- User, userID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/users/%d", config.APIURL, userID)
+	response, erro := requests.MakeAuthRequest(r, http.MethodGet, url, nil)
+	if erro != nil {
+		channel <- User{}
+		return
+	}
+	defer response.Body.Close()
 
+	var user User
+	if erro = json.NewDecoder(response.Body).Decode(&user); erro != nil {
+		channel <- User{}
+		return
+	}
+
+	channel <- user
 }
 
-func GetFollowers(channel <- chan []User, userID uint64, r *http.Request) {
+// GetFollowers chama a API para buscar os seguidores do usuário
+func GetFollowers(channel chan<- []User, userID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/users/%d/followers", config.APIURL, userID)
+	response, erro := requests.MakeAuthRequest(r, http.MethodGet, url, nil)
+	if erro != nil {
+		channel <- nil
+		return
+	}
+	defer response.Body.Close()
 
+	var followers []User
+	if erro = json.NewDecoder(response.Body).Decode(&followers); erro != nil {
+		channel <- nil
+		return
+	}
+
+	channel <- followers
 }
 
-func GetFollowing(channel <- chan []User, userID uint64, r *http.Request) {
+// GetFollowing chama a API para buscar os usuários seguidos por un usuário
+func GetFollowing(channel chan<- []User, userID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/users/%d/following", config.APIURL, userID)
+	response, erro := requests.MakeAuthRequest(r, http.MethodGet, url, nil)
+	if erro != nil {
+		channel <- nil
+		return
+	}
+	defer response.Body.Close()
 
+	var following []User
+	if erro = json.NewDecoder(response.Body).Decode(&following); erro != nil {
+		channel <- nil
+		return
+	}
+
+	channel <- following
 }
 
-func GetPublications(channel <- chan []Publication, userID uint64, r *http.Request) {
+//GetPublications chama a API para buscar as publicações de um usuário
+func GetPublications(channel chan<- []Publication, userID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/users/%d/publications", config.APIURL, userID)
+	response, erro := requests.MakeAuthRequest(r, http.MethodGet, url, nil)
+	if erro != nil {
+		channel <- nil
+		return
+	}
+	defer response.Body.Close()
 
+	var publications []Publication
+	if erro = json.NewDecoder(response.Body).Decode(&publications); erro != nil {
+		channel <- nil
+		return
+	}
+
+	channel <- publications
 }
